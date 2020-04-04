09b98c644dcbbb055083f8d587bbf8af8b23c176
pandu9aga <57618528+pandu9aga@users.noreply.github.com>
2020-04-04 15:05:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exkost/OpenActivity.java
@@ -0,0 +1,22 @@
+package com.example.exkost;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OpenActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // langsung pindah ke MainActivity atau activity lain
+        // begitu memasuki splash screen ini
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
+
