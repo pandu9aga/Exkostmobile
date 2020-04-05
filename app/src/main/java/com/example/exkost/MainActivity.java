8412504f7096a83ff911a0e52c0a527d88096fc5
pandu9aga <57618528+pandu9aga@users.noreply.github.com>
2020-04-05 14:20:23 +0700

    ganti

--- app/src/main/java/com/example/exkost/MainActivity.java
+++ app/src/main/java/com/example/exkost/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
 
 // get the reference of Button's
         firstFragment = (Button) findViewById(R.id.firstFragment);
