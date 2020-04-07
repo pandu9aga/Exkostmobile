8224a6a38423b8b0b6200310c991012b92aff591
pandu9aga <57618528+pandu9aga@users.noreply.github.com>
2020-04-07 19:04:38 +0700

    search_menu
    menu pencarian

--- app/src/main/java/com/example/exkost/HomeActivity.java
+++ app/src/main/java/com/example/exkost/HomeActivity.java
@@ -3,10 +3,13 @@ package com.example.exkost;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.SearchView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -45,7 +48,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         navigationView.getMenu().getItem(0).setChecked(true);
         firstFragmentDisplay(R.id.nav_home);
     }
-
+//optionmenu start
     private void firstFragmentDisplay(int itemId) {
 
         fragment = null;
@@ -86,4 +89,27 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+//optionmenu end
+
+//searchmenu start
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchmenu, menu);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+//searchmenu end
 }
