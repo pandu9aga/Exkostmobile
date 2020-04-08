4edbbb23245f16fb8e19e2c23de10602899258a2
arfanio <58145207+arfanio@users.noreply.github.com>
2020-04-08 09:58:26 +0700

    saldo sementara
    coba di coment kurang apa

--- app/src/main/java/com/example/exkost/BtmmenuSaldo.java
+++ app/src/main/java/com/example/exkost/BtmmenuSaldo.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
@@ -24,7 +28,18 @@ public class BtmmenuSaldo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.content_saldo, container, false);
+        View rootView = inflater.inflate(R.layout.content_saldo, container, false);
+        ListView listview =(ListView) rootView.findViewById(R.id.list);
+        final String[] items = new String[] {"+ Rp 50.000", "+ Rp 100.000", "+ Rp 100.000"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (getActivity(), android.R.layout.simple_list_item_1, items);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity().getApplicationContext(), items[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
     }
 
 }
